addc669026f27ab505f86977ea3cb57d6d69623d
Ubuntums <serhiy@ubuntu.localdomain>
2012-01-09 05:51:46 -0800

    change attachments

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,4 +1,5 @@
 class Document < ActiveRecord::Base
 	has_many :attachments
+	attr_accessible :title, :description, :attachments_attributes
 	accepts_nested_attributes_for :attachments, :allow_destroy => true, :reject_if => :all_blank
 end
